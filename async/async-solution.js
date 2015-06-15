1463b299bb8ec98c2ff593596f8ff987800e022d
Suchit <rajrikhy2@gmail.com>
2015-06-15 21:28:26 +0200

    cleaned up the code a bit, added a check for async sequential

--- async/async-solution.js
+++ async/async-solution.js
@@ -2,17 +2,19 @@ var async = {
   sequence: sequenceFunction
 }
 
-
 // sequence
-function sequenceFunction(/* async function w/ cb second */) {
-  var functionArray = Array.prototype.slice.apply(arguments[0]);
-  var current = 0;  // currently called function
-  var finalCallback = null;
+function sequenceFunction(/* [foo1, foo2, foo3...] */) {
+  var functionArray = Array.prototype.slice.apply(arguments[0]);  // functions to be called sequentially
+  var current = 0;  // currently called function index
+  var finalCallback = null; // the final callback function
 
   // iterator function
   var next = function (err, result) {
     if (current == functionArray.length || err) {
-      finalCallback(err, result);
+      if (finalCallback) {
+        finalCallback(err, result);
+      }
+
       return;
     }
     functionArray[current](next, result);
@@ -20,7 +22,6 @@ function sequenceFunction(/* async function w/ cb second */) {
   };
 
   return function (final) {
-    // functionArray.push(final);
     finalCallback = final;
     next();
   };
